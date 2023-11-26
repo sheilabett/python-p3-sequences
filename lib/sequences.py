5c8bd9a70be54eea9d3c8ab449afa9d2c94500be
sheilabett <sheilahbett@gmail.com>
2023-11-26 21:27:15 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,14 @@
 
 def print_fibonacci(length):
     pass
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+
+    if length == 0:
+        print('[]')
+    else:
+        print(fib_sequence)
+print_fibonacci(0)
